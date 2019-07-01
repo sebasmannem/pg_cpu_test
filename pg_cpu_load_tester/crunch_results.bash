75b7ed043ae9903417596723016523cebb6d3df2
Sebastiaan Mannem <sebastiaan.mannem@enterprisedb.com>
2019-07-01 17:13:04 +0200

    Added summary output

--- pg_cpu_load_tester/crunch_results.bash
+++ pg_cpu_load_tester/crunch_results.bash
@@ -1,2 +1,3 @@
 grep PCL_PARALLEL ../logs/*/env | sed 's/\/env:.*=/ /' | while read l; do ln -sf $l; done
 find -L . -name 'pg_cpu_load_*' | while read f; do echo -n "$f:"; awk '/Total tps:/{a+=$3;b+=1}END{print(a/b)}' $f; done | sed 's/\.\///;s/\.log//;s/[/:]/ /g;s/pg_cpu_load_//' | awk '{printf("%-35s %3s %7.1f\n",$2,$1,$3)}' | LC_ALL=c sort
+find . -name pg_cpu_load*.log | xargs ./svg_plotter.py
--- pg_cpu_load_tester/svg_plotter.py
+++ pg_cpu_load_tester/svg_plotter.py
@@ -4,35 +4,52 @@ import numpy as np
 import re
 import sys
 import os
+import statistics
 
 import argparse
+
+def parse_arg(argname, parsed, choices):
+    if parsed == 'all':
+        return choices
+    invalid = [arg  for arg in parsed.split(',') if arg not in choices]
+    if invalid:
+        raise Exception(argname, 'has invalid arguments', ', '.format(parsed))
+    return choices
+
+PCL_TESTS=[ 'baseline', 'no_fsync', 'tmpfs', 'wal_tmpfs', 'no_fsync_tmpfs' ]
+PCL_TYPES=[ 'empty', 'simple', 'temp_read', 'temp_write', 'read', 'write' ]
+PCL_MODES=[ 'direct', 'prepared', 'transactional', 'prepared_transactional' ]
 
 parser = argparse.ArgumentParser(description='Summarize results and graph into svg')
 parser.add_argument('files', metavar='N', nargs='+', help='the files to proces')
 parser.add_argument('-c', '--convolve', type=int, default=1, help='the files to proces')
+parser.add_argument('-t', '--tests', default='all', help='The tests to add in summary')
+parser.add_argument('-q', '--querytypes', default='all', help='The tests to add in summary')
+parser.add_argument('-m', '--modes', default='all', help='The tests to add in summary')
+parser.add_argument('-r', '--recreate', action='store_true', help='Recreate all graphs')
+
 args = parser.parse_args()
+SUM_TESTS=parse_arg('--tests', args.tests, PCL_TESTS)
+SUM_QTYPES=parse_arg('--querytypes', args.querytypes, PCL_TYPES)
+SUM_MODES=parse_arg('--modes', args.modes, PCL_MODES)
 
 #2019-06-29 01:45:00.928474        1.088000     3881.492   7762985.000       4609.667            0.000
 VALIDATOR_RE=re.compile('^[0-9]{4}(-[0-9]{2}){2} ([0-9]{2}:){2}[0-9]{2}(\.[0-9]+)?([ \t]+[0-9.]+)*$')
 SPLITTER_RE=re.compile('[ \t]+')
 PCL_PARALLEL_RE=re.compile('PCL_PARALLEL=([0-9]+)')
 #logs.default/1561767352/pg_cpu_load_empty_transactional.log
-PCL_TESTS='(baseline|no_fsync|tmpfs|wal_tmpfs|no_fsync_tmpfs)'
-PCL_TYPES="(empty|simple|temp_read|temp_write|read write)"
-PCL_MODES="(direct|prepared|transactional|prepared_transactional)"
 
-PCL_INFO_RE=re.compile('logs.{0}/[0-9]+/pg_cpu_load_{1}_{2}.log$'.format(PCL_TESTS, PCL_TYPES, PCL_MODES))
-
-thread_tps_list = []
-pg_tps_list = []
+PCL_INFO_RE=re.compile('logs.({0})/[0-9]+/pg_cpu_load_({1})_({2}).log$'.format('|'.join(PCL_TESTS), '|'.join(PCL_TYPES), '|'.join(PCL_MODES)))
 
 plots_info = {}
 
+print('test           querytype  query_mode          threads  thread_avg  thread_std      pg_avg    pg_stdev')
 for filepath in args.files:
     filedir = os.path.dirname(filepath)
     envfile = os.path.join(filedir, 'env')
+
     if not os.path.exists(envfile):
-        print('{} does not exist. Skipping {}.'.format(envfile, filepath))
+        sys.stderr.write('{} does not exist. Skipping {}.\n'.format(envfile, filepath))
         continue
     try:
         plot_info = plots_info[filedir]
@@ -51,8 +68,11 @@ for filepath in args.files:
         plot_info['PCL_TYPE'] = m.group(2)
         plot_info['PCL_MODE'] = m.group(3)
     else:
-        print('File {} does not match re {}. SKipping...'.format(filepath, PCL_INFO_RE))
+        sys.stderr.write('File {} does not match re {}. SKipping...\n'.format(filepath, PCL_INFO_RE))
         continue
+
+    thread_tps_list = []
+    pg_tps_list = []
 
     with open(filepath) as filelines:
         for line in  filelines:
@@ -68,10 +88,21 @@ for filepath in args.files:
                 print(cols[4], cols[5])
                 raise(err)
 
-    plt.figure()
-    plt.title('{PCL_TEST}: {PCL_TYPE}, {PCL_MODE} ({PCL_PARALLEL} threads)'.format(**plot_info))
-    plt.plot(np.convolve(thread_tps_list, np.ones((args.convolve,))/args.convolve, mode='valid'), label='threads_tps')
-    plt.plot(np.convolve(pg_tps_list, np.ones((args.convolve,))/args.convolve, mode='valid'), label='pg_tps')
-    plt.axis('auto')
-    plt.legend()
-    plt.savefig(filepath+".svg")
+    plot_info['THREAD_AVG'] = sum(thread_tps_list) / len(thread_tps_list)
+    plot_info['THREAD_STDEV'] = statistics.stdev(thread_tps_list)
+    plot_info['PG_AVG'] = sum(pg_tps_list) / len(pg_tps_list)
+    plot_info['PG_STDEV'] = statistics.stdev(pg_tps_list)
+    plot_info['PCL_PARALLEL'] = int(plot_info['PCL_PARALLEL'])
+    print('{PCL_TEST:14s} {PCL_TYPE:10s} {PCL_MODE:22s} {PCL_PARALLEL:4d} {THREAD_AVG:11.3f} {THREAD_STDEV:11.3f} {PG_AVG:11.3f} {PG_STDEV:11.3f}'.format(**plot_info))
+
+    if args.recreate or not os.path.exists(filepath+".svg"):
+        # Initialize the graph
+        fig = plt.figure()
+        subplot_title = '{PCL_TEST}: {PCL_TYPE}, {PCL_MODE} ({PCL_PARALLEL} threads)'.format(**plot_info)
+        ax = fig.add_subplot(autoscale_on=True, title=subplot_title)
+        ax.plot(thread_tps_list, label='threads_tps')
+        ax.plot(pg_tps_list, label='pg_tps')
+        ax.set_xlabel('Sec')
+        ax.set_ylabel('Tps')
+        ax.legend()
+        fig.savefig(filepath+".svg")
